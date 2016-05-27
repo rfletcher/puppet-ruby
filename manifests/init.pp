0564f01a6e74cd77f11f6fa2c7c91e148b20a9e0
Rick Fletcher <fletch@pobox.com>
2016-05-27 11:54:04 -0400

    Support installation of multiple ruby versions

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,6 +12,11 @@
 #
 #   String. The version to install.
 #
+# [*versions*]
+#
+#   Hash. One or more name => param pairs to install. Meant for hiera-driven,
+#   installations.
+#
 # === Examples
 #
 #  class { 'ruby':
@@ -24,16 +29,44 @@
 #
 # === Copyright
 #
-# Copyright 2014 Rick Fletcher
+# Copyright 2016 Rick Fletcher
 #
 class ruby (
-  $version,
-  $default = false,
+  $default  = false,
+  $version  = undef,
+  $versions = undef,
 ) {
   include rbenv
 
-  rbenv::plugin { 'sstephenson/ruby-build': }
-  rbenv::build { $version:
-    global => $default,
+  if $version and $versions {
+    fail( 'Specify $version or $versions, not both' )
   }
+
+  if $version != "" {
+    validate_string( $version )
+
+    if ! is_bool( $default ) {
+      fail( 'When specifying $version, $default must be true or false' )
+    }
+
+    $real_versions = {
+      "${version}" => {
+        "default" => $default
+      }
+    }
+  }
+
+  if $versions != "" {
+    validate_hash( $versions )
+
+    if $default != false and $default != "" {
+      fail( 'When specifying $versions, $default must be omitted (or false)' )
+    }
+
+    $real_versions = $versions
+  }
+
+  rbenv::plugin { 'sstephenson/ruby-build': } -> Rbenv::Build <| |>
+
+  create_resources( 'rbenv::build', $real_versions )
 }
